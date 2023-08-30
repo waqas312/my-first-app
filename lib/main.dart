86d3b64be055534d4719a64dd1fc46074e9ac7bf
waqas <waqaskhizar312@gmail.com>
2023-08-30 01:28:27 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fluter App"),
+          centerTitle: true,
+        ),
+        body:
+
+        Text("This is Body")
+        ,
+      ),
+    );
+
+  }
+}
+
+
